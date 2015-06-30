6e6664306197f30184c296a82d1c25f2d55b8f6d
vaiolis <harrisonllin@gmail.com>
2015-06-29 18:12:26 -0700

    Created placeholders for index.php
    Also copied bootstrap css over to the css directory.

---
+++ index.php
@@ -0,0 +1,19 @@
+// Include header (put css in header?)
+
+<head>
+
+</head>
+
+<body>
+
+// Write a better body
+
+<h1>A Title</h1>
+
+<p>A paragraph</p>
+
+</body>
+
+// Include sidebar
+
+// Include footer (put css in footer?)
